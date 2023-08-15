0d57de4d03dd364c4a962f932d550e7395d598d8
mikewang68 <wdg@djtu.edu.cn>
2023-08-15 21:25:22 +0800

    modify folder

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- test
+
+local opt = vim.opt
+opt.clipboard = "unnamedplus"
+opt.wrap = true
+-- opt.clipboard = ""
+
+-- opt.spelllang = "en,cjk"
+-- opt.spelloptions = "camel"
+-- opt.scrolloff = 5
+-- opt.indentexpr = ""
+opt.foldmethod = "indent"
+-- opt.foldlevel = 99
+opt.foldenable = false
+-- opt.foldlevelstart = 99
