a3f3ebfc773724efdacf0884926fd0bbb3cdfb1a
mikewang68 <wdg@djtu.edu.cn>
2024-01-29 10:00:13 +0800

    add go run autoexec

--- init.lua
+++ init.lua
@@ -1,2 +1,24 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("code_runner").setup({
+  filetype = {
+    java = {
+      "cd $dir &&",
+      "javac $fileName &&",
+      "java $fileNameWithoutExt",
+    },
+    python = "python3 -u",
+    typescript = "deno run",
+    rust = {
+      "cd $dir &&",
+      "rustc $fileName &&",
+      "$dir/$fileNameWithoutExt",
+    },
+    go = {
+      "cd $dir &&",
+      -- "go run $dir/main.go",
+      "go run $fileName &&",
+      -- "$dir/$fileNameWithoutExt",
+    },
+  },
+})
